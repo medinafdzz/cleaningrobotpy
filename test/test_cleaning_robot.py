4352073108d27e94c85c8599d4a7ba67a462eb31
Medinafdzz <suso121163@gmail.com>
2024-11-29 22:50:43 +0100

    Red phase - US2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -22,4 +22,15 @@ class TestCleaningRobot(TestCase):
         robot.heading = 'N'
         self.assertEqual(robot.robot_status(), '(0,0,N)')
 
+    @patch.object(IBS, 'get_charge_left')
+    @patch.object(GPIO, 'output')
+    def test_manage_cleaning_system_led_when_charge_left_is_greater_than_10(self, mock_gpio_output,
+                                                                            mock_get_charge_left):
+        robot = CleaningRobot()
+
+        mock_get_charge_left.return_value = 20
+        robot.manage_cleaning_system()
+        self.assertTrue(robot.cleaning_system_on)
+        self.assertFalse(robot.recharge_led_on)
+        mock_gpio_output.assert_has_calls([call(robot.CLEANING_SYSTEM_PIN, True), call(robot.RECHARGE_LED_PIN, False)], any_order=True)
 
